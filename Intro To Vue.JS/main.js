076f3d0a25b93ac21a5782cabffdc2d4182e20a7
chris <chrisshields88@gmail.com>
2018-05-09 07:10:11 -0400

    Update
    Completed Lessons 1&2

---
+++ Intro To Vue.JS/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "100% Wool Blend, Moisture Wicking",
+    image:
+      "https://dl.dropboxusercontent.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
+    link: "http://www.google.com"
+  }
+});
