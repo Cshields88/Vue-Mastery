40f91a50bd3830c91a2bce96107f134d61737659
chris <chrisshields88@gmail.com>
2018-05-10 06:49:16 -0400

    Lessons Complete
    Completed Lessons 2-5

--- Intro To Vue.JS/main.js
+++ Intro To Vue.JS/main.js
@@ -5,6 +5,20 @@ var app = new Vue({
     description: "100% Wool Blend, Moisture Wicking",
     image:
       "https://dl.dropboxusercontent.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0",
-    link: "http://www.google.com"
+    link: "http://www.google.com",
+    inventory: 20,
+    onSale: true,
+    details: ["80% Cotton", "20% Polyester", "Gender-neutral"],
+    sizes: ["Small", "Medium", "Large", "X-Large", "XX-Large"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "Green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "Blue"
+      }
+    ]
   }
 });
